0aa4dda644745484ece083ecc5ae174544f96643
balckowl <shiggyjr1492@gmail.com>
2025-03-02 04:26:06 +0900

    fix: 環境変数にVITEを追加

--- db/index.ts
+++ db/index.ts
@@ -1,19 +1,7 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 
-if (typeof process !== 'undefined' && process.env) {
-  config({ path: '.env' });
-}
-
-const TURSO_CONNECTION_URL = typeof process !== 'undefined' && process.env
-  ? process.env.TURSO_CONNECTION_URL
-  : import.meta.env.TURSO_CONNECTION_URL;
-
-const TURSO_AUTH_TOKEN = typeof process !== 'undefined' && process.env
-  ? process.env.TURSO_AUTH_TOKEN
-  : import.meta.env.TURSO_AUTH_TOKEN;
-
 export const db = drizzle({ connection: {
-  url: TURSO_CONNECTION_URL!,
-  authToken: TURSO_AUTH_TOKEN!,
+  url: import.meta.env.VITE_TURSO_CONNECTION_URL!,
+  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN!,
 }});
