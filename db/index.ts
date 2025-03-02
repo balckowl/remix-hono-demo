62d562143848d3ede58f9fb7b1c0c778bba79707
balckowl <shiggyjr1492@gmail.com>
2025-03-02 14:55:25 +0900

    修正

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
+config({ path: '.env' });
 
 export const db = drizzle({ connection: {
   url: process.env.TURSO_CONNECTION_URL!,
