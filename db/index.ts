a80066552a296bdcb93b6de1fe8e10dbc1a479e1
balckowl <shiggyjr1492@gmail.com>
2025-03-02 21:36:48 +0900

    fix

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,4 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
-config({ path: '.env' });
 
 export const drizzleClient = (url: string, authToken: string) => {
   const db = drizzle({
